7146a852de5acc7ba8ab26b5a6fad288dac66f9d
Andre Wiggins <andrewiggins@live.com>
2023-04-23 22:27:41 -0700

    Rect area with structs

--- projects/ch05_structs/src/main.rs
+++ projects/ch05_structs/src/main.rs
@@ -1,12 +1,20 @@
+struct Rect {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    let rect1 = (30, 50);
+    let rect1 = Rect {
+        height: 30,
+        width: 50,
+    };
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(rect1)
+        area(&rect1)
     );
 }
 
-fn area(rect: (u32, u32)) -> u32 {
-    rect.0 * rect.1
+fn area(rect: &Rect) -> u32 {
+    rect.width * rect.height
 }
